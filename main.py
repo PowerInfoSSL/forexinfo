e1b0982a726375233d603c62937b14041e3aacaf
Rasool Ziafaty <r.ziafaty@hotmail.com>
2020-03-27 16:15:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,34 @@
-from urllib.request import urlopen
+#!/usr/bin/python3
 import requests
-import json
-import sys
+from time import sleep
+from datetime import datetime as dt
+import os
 
+my_price = 1.10314
+currency_pair = 'EUR/USD'
 
-
-
-
-my_price=1.10314
-
-
-
-
+api_v3_forex_url = "https://financialmodelingprep.com/api/v3/forex"
 
 def get_jsonparsed_data(url):
     response = requests.get(url)
     return response.json()
 
-
-#import pprint
-url = ("https://financialmodelingprep.com/api/v3/forex")
-from time import sleep
-import sys,os
-from datetime import datetime as dt
-
 def range():
-    while 1:
-        time=dt.now().strftime('%H:%M:%S')
-        h=get_jsonparsed_data(url)
-        #print(h)
-        s=h['forexList']
-        for i in s:
-            if i['ticker'] == 'EUR/USD':
-                su=str(float(i['ask']) - float(my_price))
-                print('\n\n\n\nTime:%s\n_____________________\nMe:' %time + str(my_price))
-                print('Cu:' + str(i['ask']) + '\n_____________________\n')
-                #
-                sleep(3)
-                #os.system("clear")
+    while True:
+        time = dt.now().strftime('%H:%M:%S')
+        try:
+            result_json = get_jsonparsed_data(api_v3_forex_url)
+            os.system('clear')
+            forex_list = result_json['forexList']
+            for element in forex_list:
+                if element['ticker'] == currency_pair:
+                    subtraction_result = str(float(element['ask']) - my_price)
+                    print('\n\n\n\nTime:%s\n_____________________\nMe:' %time + str(my_price))
+                    #print("subtraction:"+subtraction_result)
+                    print('Cu:' + str(element['ask']) + '\n_____________________\n')
+        except Exception as e:
+            print (str(e))
+        sleep(3)
 
-
-while 1:
-    try:
-        range()
-    except:
-        print('Error')
+if __name__ == '__main__':
+    range()

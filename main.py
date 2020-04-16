c4de2028ecc02c9413f46bb80e734cf6300fb18f
Jafar Tavana <PowerInfoSSL@gmail.com>
2020-04-16 10:01:36 +0430

    Update main.py
    Show min and max value and ask for which price point

--- main.py
+++ main.py
@@ -1,34 +1,80 @@
-#!/usr/bin/python3
+from urllib.request import urlopen
 import requests
-from time import sleep
-from datetime import datetime as dt
-import os
+import json
+import sys
+#from termcolor import colored
 
-my_price = 1.10314
-currency_pair = 'EUR/USD'
 
-api_v3_forex_url = "https://financialmodelingprep.com/api/v3/forex"
+
+#------------------Define Point to calculate
+my_price=float(0)
+while 1:
+    q=input('\n\n\n    Enter your point:')
+    if q!="":
+        my_price=float(q)
+        print('\nYour current point is %.5f' %float(q))
+        break
+    else:
+        print('Enter Your sell point')
+
+
+
+position_type='sell'
+
+
+
 
 def get_jsonparsed_data(url):
     response = requests.get(url)
     return response.json()
 
-def range():
-    while True:
-        time = dt.now().strftime('%H:%M:%S')
-        try:
-            result_json = get_jsonparsed_data(api_v3_forex_url)
-            os.system('clear')
-            forex_list = result_json['forexList']
-            for element in forex_list:
-                if element['ticker'] == currency_pair:
-                    subtraction_result = str(float(element['ask']) - my_price)
-                    print('\n\n\n\nTime:%s\n_____________________\nMe:' %time + str(my_price))
-                    #print("subtraction:"+subtraction_result)
-                    print('Cu:' + str(element['ask']) + '\n_____________________\n')
-        except Exception as e:
-            print (str(e))
-        sleep(3)
 
-if __name__ == '__main__':
-    range()
+#import pprint
+url = ("https://financialmodelingprep.com/api/v3/forex")
+from time import sleep
+import sys,os
+from datetime import datetime as dt
+
+
+
+def range():
+    min_c=float(2)
+    max_c=float(0)
+    while 1:
+        global n_num
+        n_num += 1
+        time=dt.now().strftime('%H:%M:%S')
+        h=get_jsonparsed_data(url)
+        #print(h)
+        s=h['forexList']
+        for i in s:
+            if i['ticker'] == 'EUR/USD':
+                su=str(float(i['ask']) - float(my_price))
+                if position_type=="sell":
+                    ww="\n" + str(float(my_price)-float(i['ask']))[0:7]
+                print('\n\n\n\nTime:%s\n_____________________\nMe:' %time + str(my_price))
+                print('Cu:' + str(i['ask']) + ww + '\n_____________________\n')
+                #
+                #
+                #os.system("clear")
+                global avrg
+                avrg +=float(i['ask'])
+                if (float(i['ask'])) > float(max_c):
+                    max_c=float(i['ask'])
+                if (float(i['ask'])) < float(min_c):
+                    min_c=float(i['ask'])
+                #if min_max ==5:
+                #
+                suum=float(avrg / n_num)
+                print("\nMAX: " + str(max_c) + " - MIN: " + str(min_c) + " - Avr: " + "%.5f" % suum)
+                #print(n_num)
+                sleep(5)
+
+
+n_num=0
+avrg=float(0)
+while 1:
+    try:
+        range()
+    except:
+        print('Error')
